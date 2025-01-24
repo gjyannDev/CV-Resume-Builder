8ae4603c1192a7f3fb1c868558ab9b15d8318f3b
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-01-24 11:00:03 +0800

    Add education and experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import Header from './components/Header'
+import GeneralInformation from './components/GeneralInfo'
+import Education from './components/Education'
+import Experience from './components/Experience'
 
 function App() {
-
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div className="main__container">
+      <Header />
+      <main className="main__contents">
+        <section className='input__form-container'>
+          <GeneralInformation />
+          <Education />
+          <Experience />
+        </section>
+      </main>
+    </div>
   )
 }
 
