f4504fee90c1b29aee955dab3586f8c3b3a89632
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-01-26 22:12:37 +0800

    Created this file for storing data

---
+++ src/components/Data.jsx
@@ -0,0 +1,28 @@
+const data = {
+  generalInfoData: {
+    fullName: "",
+    mobileNum: "",
+    email: "",
+    portfolio: "",
+    address: "",
+  },
+
+  educationData: {
+    schoolName: "",
+    courseName: "",
+    address: "",
+    startDate: "",
+    endDate: "",
+  },
+
+  experienceData: {
+    companyName: "",
+    positionTitle: "",
+    startDate: "",
+    endDate: "",
+    location: "",
+    jobDescription: "",
+  },
+};
+
+export default data;
